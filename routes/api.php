f779cb8fa05e6943e5c1fdeb006dc06b3e708d89
a831123c <a831123d@gmail.com>
2020-06-20 15:29:15 +0800

    feat: add register route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('api')->group(function () {
+    Route::apiResource('register', 'RegisterController')->only('store');
 });

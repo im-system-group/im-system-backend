189ad7367accc058efa08ad28c4d09d516b4e8b0
a831123c <a831123d@gmail.com>
2020-07-15 16:05:06 +0800

    style: rename LoginController to AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,20 +7,27 @@ namespace App\Http\Controllers;
 use App\Http\Requests\LoginRequest;
 use App\Http\Resources\TokenResource;
 use App\Member;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
-class LoginController extends Controller
+class AuthController extends Controller
 {
     public function store(LoginRequest $request)
     {
         $member = Member::where('account', $request->account)->first();
 
-        if(!Hash::check($request->password, $member->password)) {
+        if(!Auth::attempt($request->only('account', 'password'))) {
             return response('', 401);
         }
 
         return (new TokenResource($member->createToken($member->name)))
             ->response()
             ->setStatusCode(201);
+    }
+
+    public function destroy()
+    {
+        $user = Auth::user();
+        $user->tokens()->where('id', $user->currentAccessToken()->id)->delete();
+        return response('', 204);
     }
 }

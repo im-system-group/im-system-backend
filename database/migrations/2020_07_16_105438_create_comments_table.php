0e89d5fb33eb5725cd0d3a9069e0dd8d4f57c9d0
a831123c <a831123d@gmail.com>
2020-07-17 16:28:07 +0800

    feat: add comments model and migration

---
+++ database/migrations/2020_07_16_105438_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('author_id');
+            $table->uuid('article_id');
+            $table->text('content');
+            $table->timestamps();
+
+            $table->foreign('author_id')->references('id')->on('members');
+            $table->foreign('article_id')->references('id')->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}

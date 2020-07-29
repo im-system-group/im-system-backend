206b646c4bbb62d0db947ae822c384213db421db
a831123c <a831123d@gmail.com>
2020-07-29 19:05:18 +0800

    feat: add member index test

---
+++ tests/Feature/Member/IndexTest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Member;
+
+use Tests\Feature\ActingLogin;
+
+class IndexTest extends ActingLogin
+{
+   public function testIndex()
+   {
+       $response = $this->getJson(route('member.index'));
+       $response->assertStatus(200);
+
+       $response->assertJson([
+           'data' => [
+               'id' => $this->member->id,
+               'account' => $this->member->account,
+               'name' => $this->member->name,
+               'email' => $this->member->email,
+               'avatar' => $this->member->avatar
+           ]
+       ]);
+   }
+}

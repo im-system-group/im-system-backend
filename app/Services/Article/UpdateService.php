0020fea6ef652f62ae01aa58bb05c125e31015b7
a831123c <a831123d@gmail.com>
2020-08-14 18:44:42 +0800

    feat: article update add upload image

--- app/Services/Article/UpdateService.php
+++ app/Services/Article/UpdateService.php
@@ -7,12 +7,25 @@ namespace App\Services\Article;
 use App\Article;
 use App\Http\Requests\Article\UpdateRequest;
 use App\Member;
+use App\Util\CanUpload;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class UpdateService
 {
+    use CanUpload;
+
     public function update(Article $article, UpdateRequest $request)
     {
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+
+            $originImage = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+
+            Storage::disk('public')->delete($article->image);
+            $article->image = $this->uploadFile($image, 'postupload', $originImage);
+        }
+
         DB::transaction(function () use ($article, $request) {
             $article->title = htmlentities($request->title, ENT_QUOTES, 'UTF-8');
             $article->content = htmlentities($request->content, ENT_QUOTES, 'UTF-8');
